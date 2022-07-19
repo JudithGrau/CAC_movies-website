a21fdec5e72bf18fd226e67e6be149d87467d6bb
JudithGrau <judith.betina.grau@gmail.com>
2022-07-19 03:41:57 -0300

    Scroll infinito y busqueda con debounce

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { Spinner } from "../components/Spinner";
+import { getMovieImg } from "../utils/getMovieImg";
 import { get } from "../utils/httpClient";
 import styles from "./MovieDetails.module.css";
 
@@ -13,8 +14,8 @@ export function MovieDetails() {
         setIsLoading(true);
 
         get("/movie/" + movieId).then((data) => {
-            setMovie(data);
-            setIsLoading(false);
+        setMovie(data);
+        setIsLoading(false);
         });
     }, [movieId]);
 
@@ -22,9 +23,9 @@ export function MovieDetails() {
         return <Spinner />;
     }
 
-    const imageUrl = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
+    const imageUrl = getMovieImg(movie.poster_path, 500);
     return (
-    <div className={styles.detailsContainer}>
+        <div className={styles.detailsContainer}>
         <img
             className={`${styles.col} ${styles.movieImage}`}
             src={imageUrl}
@@ -32,16 +33,16 @@ export function MovieDetails() {
         />
         <div className={`${styles.col} ${styles.movieDetails}`}>
             <p className={styles.firstItem}>
-                <strong>Title:</strong> {movie.title}
+            <strong>Title:</strong> {movie.title}
             </p>
             <p>
-                <strong>Genres:</strong>{" "}
-                {movie.genres.map((genre) => genre.name).join(", ")}
+            <strong>Genres:</strong>{" "}
+            {movie.genres.map((genre) => genre.name).join(", ")}
             </p>
             <p>
-                <strong>Description:</strong> {movie.overview}
+            <strong>Description:</strong> {movie.overview}
             </p>
         </div>
-    </div>
+        </div>
     );
 }
